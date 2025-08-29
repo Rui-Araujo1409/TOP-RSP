f3f88498afdda62e02d97868acc022fe9a674efd
Rui-Araujo1409 <rui1409@gmail.com>
2025-08-29 14:44:17 +0000

    fx playGame

--- script.js
+++ script.js
@@ -1,55 +1,92 @@
+//criar a jogada da CPU
 function getComputerChoice() {
     let jogadaCPU = "";
-    let valorAleatório = Math.floor(Math.random()*3) + 1;
-    if(valorAleatório == 1){
+    //criar var com random que irá definir as escolhas possíveis   
+    let valorAleatório = Math.floor(Math.random() * 3) + 1;
+    //se for 1 será rock
+    if (valorAleatório == 1) {
         jogadaCPU = "rock";
-    }else if(valorAleatório == 2){
+        //se for 2 será paper
+    } else if (valorAleatório == 2) {
         jogadaCPU = "paper";
-    }else{
+        //se for 3 será scissor
+    } else {
         jogadaCPU = "scissors";
     }
+    //retornar valor jogada
     return jogadaCPU;
 }
 
-const computerSelection = getComputerChoice();
-
-
-function getHumanChoice(){
+//fx para a jogada pessoa
+function getHumanChoice() {
+    //criar var que vai guardar input da pessoa
     let jogadaPessoa = prompt("Qual a sua jogada? ");
+    //converter texto em minusculas 
     jogadaPessoa = jogadaPessoa.toLowerCase();
+    //retornar
     return jogadaPessoa;
 }
 
-const humanSelection = getHumanChoice();
-console.log(`A jogada da CPU foi ${computerSelection}`);
-console.log(`A jogada da pessoa foi ${humanSelection}`);
 
-let humanScore = 0;
-let computerScore = 0;
-
-function playRound(humanChoice,computerChoice){
-if(humanChoice == "rock" && computerChoice == "paper") {
-    console.log("You lose. Paper beats Rock.");
-    computerScore++;
-} else if(humanChoice == "rock" && computerChoice == "scissors"){
-    console.log("You win. Rock beats Scissors.");
-    humanScore++;
-} else if(humanChoice == "paper" && computerChoice == "rock"){
-    humanScore++;
-    console.log("You win. Paper beats Rock.");
-} else if(humanChoice == "paper" && computerChoice == "scissors"){
-    console.log("You lose. Scissors beats Paper.");
-    computerScore++;
-} else if(humanChoice == "scissors" && computerChoice == "rock"){
-    console.log("You lose. Rock beats Scissors.");
-    computerScore++;
-} else if(humanChoice == "scissors" && computerChoice == "paper"){
-    console.log("You win. Scissors beats Paper.");
-    humanScore++;
+//criar vars para guardar resultados
+   //criar var resultadoCPU com valorIn 0
+    let humanScore = 0;
+    //criar var resultadopessoa com valorIn 0
+    let computerScore = 0;
+//fx para avaliação jogada
+function playRound(humanChoice, computerChoice) {
+    //comparar as vars das jogadas
+    if (humanChoice == "rock" && computerChoice == "paper") {
+        //adicionar 1 ao jogador vencedor
+        computerScore++;
+        //print da mensagem do resultado jogada
+        console.log(`You lose. Paper beats Rock.`);
+    } else if (humanChoice == "rock" && computerChoice == "scissors") {
+        humanScore++;
+        console.log(`You win. Rock beats Scissors.`);
+    } else if (humanChoice == "paper" && computerChoice == "rock") {
+        humanScore++;
+        console.log(`You win. Paper beats Rock.`);
+    } else if (humanChoice == "paper" && computerChoice == "scissors") {
+        computerScore++;
+        console.log(`You lose. Scissors beats Paper.`);   
+    } else if (humanChoice == "scissors" && computerChoice == "rock") {
+        computerScore++;
+        console.log(`You lose. Rock beats Scissors.`);    
+    } else if (humanChoice == "scissors" && computerChoice == "paper") {
+        humanScore++;
+        console.log(`You win. Scissors beats Paper.`); 
+    } else if (humanChoice == "scissors" && computerChoice == "scissors") {
+        console.log(`Tie. Both choose Scissors.`);
+    } else if (humanChoice == "paper" && computerChoice == "paper") {
+        console.log(`Tie. Both choose Paper.`);
+    } else if (humanChoice == "rock" && computerChoice == "rock") {
+        console.log(`Tie. Both choose Rock.`);
+    }
+    return;
 }
-return;
-}
 
-playRound(humanSelection,computerSelection);
-console.log(humanScore);
-console.log(computerScore);
+//fx que vai chamar a jogada e guardar/apresentar resultados (5 voltas)
+ function playGame(n) {
+    //criar o loop
+    for (i = 1; i <= n; i++) {
+        //var que chama a jogada da pessoa
+        let humanSelection = getHumanChoice();
+        //var que chama a jogada da CPU
+        let computerSelection = getComputerChoice();
+        //chamar a fx da jogada
+        playRound(humanSelection, computerSelection);
+    }
+    //compara os resultados finais
+    if(computerScore > humanScore) {
+        //apresenta vencedor do jogo e resultados finais
+        console.log(`CPU wins the game. Final score: you: ${humanScore}, CPU: ${computerScore}`);
+    } else if(computerScore < humanScore) {
+        console.log(`You win the game. Final score: you: ${humanScore}, CPU: ${computerScore}`);
+    } else {
+        console.log(`A tie. Final score: you: ${humanScore}, CPU: ${computerScore}`);
+    }
+};
+
+
+playGame(5);
